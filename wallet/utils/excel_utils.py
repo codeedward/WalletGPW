b967f080290b794f773dd4a0186576987cf87a87
codeedward <mocarski.contact@gmail.com>
2019-04-21 16:05:47 +0100

    Move logic to upload file separate script.

--- wallet/utils/excel_utils.py
+++ wallet/utils/excel_utils.py
@@ -1,12 +1,31 @@
+import openpyxl
+from ..models import ExcelEntryRow
+
+def ReadExcel(excel_file):
+    # you may put validations here to check extension or file size
+    wb = openpyxl.load_workbook(excel_file)
+
+    # getting a particular sheet by name out of many sheets
+    worksheet = wb["Sheet1"]
+    #print(worksheet)
+
+    excel_data = list()
+    # iterating over the rows and
+    # getting value from each cell in row
+    for row in worksheet.iter_rows():
+        row_data = ExcelEntryRow(row)
+        #print(row_data.date)
+        #for cell in row:
+            #row_data.append(str(cell.value))
+        excel_data.append(row_data)
+    return excel_data
+
 def FilterExcel(listOfExcelRows, accountType):
     return filter(lambda x: filterExcel(x, accountType), listOfExcelRows)
 
 
 def filterExcel(excelEntryRow, accountType):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-
     if(excelEntryRow.accountType != accountType and accountType != ''):
         return False
-
 
     return True
--- wallet/views.py
+++ wallet/views.py
@@ -1,31 +1,13 @@
 from django.shortcuts import render
-import openpyxl
 from .models import ExcelEntryRow
-from .utils.excel_utils import FilterExcel
+from .utils.excel_utils import FilterExcel, ReadExcel
 
 def Dashboard(request):
     myData = { 'dataTest': 'its just test'}
     if "GET" == request.method:
         return render(request, 'wallet/dashboard.html', {})
     else:
-        excel_file = request.FILES["excel_file"]
-        # you may put validations here to check extension or file size
-        wb = openpyxl.load_workbook(excel_file)
-
-        # getting a particular sheet by name out of many sheets
-        worksheet = wb["Sheet1"]
-        print(worksheet)
-
-        excel_data = list()
-        # iterating over the rows and
-        # getting value from each cell in row
-        for row in worksheet.iter_rows():
-            row_data = ExcelEntryRow(row)
-            print(row_data.date)
-            #for cell in row:
-                #row_data.append(str(cell.value))
-            excel_data.append(row_data)
-
+        excel_data = ReadExcel(request.FILES["excel_file"])
         modelData = FilterExcel(excel_data, 'Normalny')
         return render(request, 'wallet/dashboard.html', {"excel_data": modelData})
     #return render(request, 'wallet/dashboard.html', myData)
