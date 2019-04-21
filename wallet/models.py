76971f72e70d26069723e7bb615aed3f29ebdf94
codeedward <mocarski.contact@gmail.com>
2019-04-21 19:08:02 +0100

    Keep loaded data from excel in the session already working somehow. Need to refactor though.

--- wallet/models.py
+++ wallet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 class ExcelEntryRow():
     date = ''
@@ -11,13 +12,27 @@ class ExcelEntryRow():
     balanceChange = ''
     accountType = ''
 
-    def __init__(self, row):
-        self.date = row[0].value
-        self.transactionType = row[1].value
-        self.name = row[2].value
-        self.quantity = row[3].value
-        self.price = row[4].value
-        self.transactionValue = row[5].value
-        self.fee = row[6].value
-        self.balanceChange = row[9].value
-        self.accountType = row[10].value
+    def __init__(self, row = None):
+        print("Typ tej klasy to:")
+        print(type(row))
+        if type(row) is tuple:
+            self.date = row[0].value
+            self.transactionType = row[1].value
+            self.name = row[2].value
+            self.quantity = row[3].value
+            self.price = row[4].value
+            self.transactionValue = row[5].value
+            self.fee = row[6].value
+            self.balanceChange = row[9].value
+            self.accountType = row[10].value
+
+        elif type(row) is dict:
+            self.date = row['date']
+            self.transactionType = row['transactionType']
+            self.name = row['name']
+            self.quantity = row['quantity']
+            self.price = row['price']
+            self.transactionValue = row['transactionValue']
+            self.fee = row['fee']
+            self.balanceChange = row['balanceChange']
+            self.accountType = row['accountType']
--- wallet/views.py
+++ wallet/views.py
@@ -1,14 +1,53 @@
 from django.shortcuts import render
 from .models import ExcelEntryRow
 from .utils.excel_utils import FilterExcel, ReadExcel
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 def Dashboard(request):
-    myData = { 'dataTest': 'its just test'}
+
     if "GET" == request.method:
+        #request.session['dataTest'] = 'someSessionData'
+
+        if('excelData' in request.session):
+
+            excelDataFromSession = request.session['excelData']
+
+            deserializedDataFromSession = []
+            for r in excelDataFromSession:
+                #print(type(r))
+                rowSet = json.loads(r)
+                newExcelObj = ExcelEntryRow(rowSet)
+                #newExcelObj.name = rowSet['name']
+                newDesObj = newExcelObj #json.loads(r, ExcelEntryRow)
+
+                deserializedDataFromSession.append(newDesObj)
+            #serializers.deserialize("xml", , cls=HandleMyClass)
+            #print("Zdeserializowane dane:")
+            #print(deserializedDataFromSession)
+            modelData = FilterExcel(deserializedDataFromSession, 'Normalny')
+            return render(request, 'wallet/dashboard.html', {"excel_data": modelData})
+
         return render(request, 'wallet/dashboard.html', {})
     else:
+        #print("Session value is here:"+request.session['dataTest'])
         excel_data = ReadExcel(request.FILES["excel_file"])
+        #print(type(excel_data))
+        #excelRow = excel_data[0]
+        serializedData = [json.dumps(r.__dict__, cls=DjangoJSONEncoder) for r in excel_data]
+        #print(serializedData)
+        #serializedData = [json.dumps(r, cls=mySerializer)  for r in excel_data]
+        request.session['excelData'] = serializedData
         modelData = FilterExcel(excel_data, 'Normalny')
         return render(request, 'wallet/dashboard.html', {"excel_data": modelData})
     #return render(request, 'wallet/dashboard.html', myData)
 
+
+'''class HandleMyClass(simplejson.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ExcelEntryRow):
+            return obj.__dict__
+        return simplejson.JSONEncoder.default(self, obj)'''
+class mySerializer(json.JSONEncoder):
+    def default(self, obj):
+        return obj.__dict__
